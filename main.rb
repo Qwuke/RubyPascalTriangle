79e68f28f6c04817ae7589d8bfe36b41cc823453
Qwuke <tristan.e.mortimer@gmail.com>
2018-01-21 20:06:08 -0500

    Unformatted triangle recursive calculation, with shorter int validation

--- main.rb
+++ main.rb
@@ -1,14 +1,32 @@
-puts "Hello World"
-isInt = false
-depth = 0
-while !isInt
-	puts "Please enter how deep to calculate the triangle: "
-	depth = $stdin.gets.chomp.to_i
-	if depth > 0
-		isInt=true
+def recursivePascal(i, j)
+	if j == 0
+		return 1
+	elsif j == i 
+		return 1
 	else
-		puts "That couldn't be read as a valid integer value!"
+		return recursivePascal(i-1, j-1) + recursivePascal(i - 1, j)
 	end
-
 end
-puts depth
+
+def putRows(d)
+	i = 0
+	while i < d
+		j = 0 
+		while j <= i
+			print recursivePascal(i,j).to_s + " "
+			$stdout.flush
+			j+=1
+		end
+		puts " "
+		i+=1
+	end
+end
+
+depth = 0
+while !(depth > 0)
+		print "Please enter a valid integer value for how deep to calculate the triangle: "
+		depth = $stdin.gets.chomp.to_i
+		$stdout.flush
+end
+
+putRows(depth)
